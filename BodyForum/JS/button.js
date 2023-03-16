498adfd287cab723620c7c4724f9e55bf9de23a3
HubertGrabda <hubertgrabda.biznes@gmail.com>
2023-03-16 20:47:46 +0100

    nav bar zrobiony

--- BodyForum/JS/button.js
+++ BodyForum/JS/button.js
@@ -8,3 +8,26 @@ function hideMenu() {
 }
 
 document.querySelector("#toggle_menu").addEventListener('click', hideMenu());
+
+var list_is_visible = false;  
+
+function hideList() {
+    const button = document.querySelector("#toggle");
+    const inner_list = document.querySelector(".inner-list");
+      
+    
+    button.addEventListener("click",function() {
+        if (list_is_visible === false) {
+            inner_list.style.display = 'block'; 
+             list_is_visible =  true;
+             return list_is_visible
+        }
+        else {
+            inner_list.style.display = 'none';
+            list_is_visible = false;
+            return list_is_visible;
+        }
+    })
+}
+
+document.querySelector("#toggle").addEventListener("click",hideList());

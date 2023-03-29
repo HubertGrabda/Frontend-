12b8a2a69ca54a2fcf0980e285a77231346d3b05
HubertGrabda <hubertgrabda.biznes@gmail.com>
2023-03-29 18:25:20 +0200

    refaktozyacja

--- BodyForum/JS/login.js
+++ BodyForum/JS/login.js
@@ -1,34 +1,35 @@
-
-   
 const email = document.querySelector("#email");
-const pass = document.querySelector("#password");
-const wrng = document.querySelector("#result");
+const password = document.querySelector("#password");
+const wrong = document.querySelector("#result");
 
-document.querySelector("#btn").addEventListener('click', login, false);
-
-function login() {
-    if (email.value.trim() !== '' && pass.value.trim() !== '') {
+const login = () => {
+    if (email.value.trim() !== '' && password.value.trim() !== '') {
         window.location.href = '../Podstrony/index-logged-in.html';
     }
     else {
-        wrng.innerHTML = "Wprowadź poprawne dane."
+        wrong.innerHTML = "Wprowadź poprawne dane."
     }
+}
+
+const hide = () => {
+    const usericon1 = document.querySelector("#emot1");
+    const usericon2 = document.querySelector("#emot2");
+    if (email.value !== ' '){
+     usericon1.remove();
+    }
+    if (password.value !== ' '){
+     usericon2.remove();
+    }
+    console.log(usericon1, usericon2);
 }
 
 const inputs = document.getElementsByClassName('input')
 
-for (let i=0; i<inputs.length; i++) {
-    inputs[i].addEventListener('click', hide, false)
+for (let i=0; i<inputs.length;) {
+    inputs[i].addEventListener('click', hide);
 }
 
 
-function hide() {
-   const usericon1 = document.querySelector('#emot1');
-   const usericon2 = document.querySelector("#emot2");
-   if (email.value !== '') {
-    usericon1.remove();
-   }
-   if (pass !== '') {
-    usericon2.remove();
-   }
-}
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector("#btn").addEventListener('click', login, false); 
+})
